7425bd8f99296abea5ab178b61e92b57fd439e35
alexdiem <alexandra.diem@gmail.com>
2022-09-27 16:03:04 +0200

    request token

--- frontend/src/StravaLogin.tsx
+++ frontend/src/StravaLogin.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import React from "react"
 import { useSearchParams } from "react-router-dom"
 
@@ -5,21 +6,34 @@ export function handleStravaLogin() {
     const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID
     const redirectUri = process.env.REACT_APP_STRAVA_REDIRECT + '/callback'
     const scope = "profile:read_all,activity:read"
+    const authrorizeUrl = `http://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${redirectUri}/exchange_token&approval_prompt=force&scope=${scope}`
+
     window.open(
-        `http://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${redirectUri}/exchange_token&approval_prompt=force&scope=${scope}`,
+        authrorizeUrl,
         "_self"
     )
   }
 
 
 export function SegmentChallenge() {
+    const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID
+    const stravaClientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET
+
     const [searchParams] = useSearchParams()
-    for (const entry of searchParams.entries()) {
-        const [param, value] = entry;
-        console.log(param, value);
-      }
+    const code = searchParams.get('code')
+    const scope = searchParams.get('scope')
+
+    const tokenRequestUrl = `https://www.strava.com/api/v3/oauth/token?client_id=${stravaClientId}&client_secret=${stravaClientSecret}&code=${code}&grant_type=authorization_code`
+    console.log(tokenRequestUrl)
+
+    let res = axios.post(tokenRequestUrl).then(function(response) {
+        return response
+    })
+    console.log(res)
     return (
-        <p>Congrats, you have successfully logged in.</p>
+        <div>
+            <p>Congrats, you have successfully logged in.</p>
+        </div>
     )
 }
 
