ad9f242bf3f58ea765ef0f8413f5ba10d5655aff
alexdiem <alexandra.diem@gmail.com>
2022-09-27 12:56:14 +0200

    fix .env use

--- frontend/src/StravaLogin.js
+++ frontend/src/StravaLogin.js
@@ -1,8 +1,7 @@
 export function StravaLogin() {
-    require('dotenv-webpack').config();
-    const stravaClientId = process.env.STRAVA_CLIENT_ID;
-    const redirectUri = process.env.STRAVA_REDIRECT;
-    const scope = "activities:read";
+    const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
+    const redirectUri = process.env.REACT_APP_STRAVA_REDIRECT;
+    const scope = "activity:read";
     return (
         window.location = `http://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${redirectUri}/exchange_token&approval_prompt=force&scope=${scope}`
     )
